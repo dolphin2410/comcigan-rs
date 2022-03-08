da072585268683e2e08d909e45ba2d9c23f76cff
dolphin2410 <dolphin2410@outlook.kr>
2022-03-08 16:14:49 +0900

    Fix structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+use std::collections::HashMap;
+
 use class::{Grade, Class, Day, SchoolData};
 use hyper::{body::HttpBody as _, Client, client::HttpConnector};
+use fancy_regex::Regex;
 use serde::{Serialize, Deserialize};
+use serde_json::Value;
 
 pub mod class;
 
@@ -14,9 +18,17 @@ pub struct School(u32, String, String, u32);
 
 #[derive(Serialize, Deserialize)]
 pub struct RawSchoolData {
-    pub 자료311: Vec<Vec<Vec<Vec<u32>>>>,
-    pub 자료565: Vec<String>,
-    pub 자료389: Vec<String>
+    pub timetable: Vec<Vec<Vec<Vec<u32>>>>,
+    pub subjects: Vec<String>,
+    pub teachers: Vec<String>
+}
+
+pub struct RawSchoolDataKey {
+    pub timetable: String,
+    pub subjects: String,
+    pub teachers: String,
+    pub encode_header: String,
+    pub url_piece: String
 }
 
 #[tokio::test]
@@ -31,10 +43,13 @@ async fn test() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 }
 
 pub async fn view(client: &Client<HttpConnector>, school: &School) -> Result<SchoolData, Box<dyn std::error::Error + Send + Sync>> {
-    let raw_id = format!("106686_{}_0_1", school.3);
+    let subtarget = request_target(client).await?;
+    let raw_id = format!("{}{}_0_1", subtarget.encode_header, school.3);
     let encoded = base64::encode(raw_id);
 
-    let request = format!("http://comci.kr:4082/163398?{}", &encoded).parse()?;
+
+    let target = subtarget.url_piece.split("?").nth(0).unwrap();
+    let request = format!("http://comci.kr:4082/{}?{}", &target, &encoded).parse()?;
     let mut response = client.get(request).await?;
 
     let mut buffer = vec![];
@@ -43,23 +58,33 @@ pub async fn view(client: &Client<HttpConnector>, school: &School) -> Result<Sch
     }
 
     let (school_list, _, _) = encoding_rs::UTF_8.decode(buffer.as_slice());
-    let data = serde_json::from_str::<RawSchoolData>(validate_json(&school_list).as_str()).unwrap();
+    let json = validate_json(&school_list);
+    let raw_data = serde_json::from_str::<HashMap<&str, Value>>(json.as_str()).unwrap();
+    let teachers = serde_json::value::from_value::<Vec<String>>(raw_data.get(subtarget.teachers.as_str()).unwrap().to_owned()).unwrap();
+    let subjects = serde_json::value::from_value::<Vec<String>>(raw_data.get(subtarget.subjects.as_str()).unwrap().to_owned()).unwrap();
+    let timetable = serde_json::value::from_value::<Vec<Vec<Vec<Vec<u32>>>>>(raw_data.get(subtarget.timetable.as_str()).unwrap().to_owned()).unwrap();
+    
+    let data = RawSchoolData {
+        teachers,
+        subjects,
+        timetable
+    };
 
     buffer.clear();
 
     let mut to_return = SchoolData { grades: vec![] };
-    for grade_index in 1..data.자료311.len() {
+    for grade_index in 1..data.timetable.len() {
         let mut grade = Grade { classes: vec![] };
-        for class_index in 1..data.자료311[grade_index].len() {
+        for class_index in 1..data.timetable[grade_index].len() {
             let mut class = Class { days: vec![] };
-            for days_index in 1..data.자료311[grade_index][class_index].len() {
+            for days_index in 1..data.timetable[grade_index][class_index].len() {
                 let mut day = Day { studies: vec![] };
-                for index in 1..data.자료311[grade_index][class_index][days_index].len() {
-                    let subj_data = data.자료311[grade_index][class_index][days_index][index];
+                for index in 1..data.timetable[grade_index][class_index][days_index].len() {
+                    let subj_data = data.timetable[grade_index][class_index][days_index][index];
                     let th = (subj_data as f32 / 100.0).floor() as u32;
                     let code = subj_data - (th * 100);
-                    let mut subject = data.자료565[code as usize].clone();
-                    let mut teacher = data.자료389[th as usize].clone();
+                    let mut subject = data.subjects[code as usize].clone();
+                    let mut teacher = data.teachers[th as usize].clone();
                     if subject == "19" {
                         subject.clear();
                         subject.push_str("Nothing to study...");
@@ -83,7 +108,9 @@ pub async fn search_school(client: &Client<HttpConnector>, school: &'static str)
     let (result, _, _) = encoding_rs::EUC_KR.encode(school);
     let query: String = result.iter().map(|byte| format!("%{:X}", byte)).collect();
 
-    let request = format!("http://comci.kr:4082/163398?72294l{}", &query).parse()?;
+    let target = request_target(client).await?;
+
+    let request = format!("http://comci.kr:4082/{}{}", &target.url_piece, &query).parse()?;
     let mut response = client.get(request).await?;
     
     let mut buffer = vec![];
@@ -98,4 +125,35 @@ pub async fn search_school(client: &Client<HttpConnector>, school: &'static str)
 
 pub fn validate_json(str: &str) -> String {
     str.chars().filter(|c| { c != &'\u{0}' }).collect::<String>()
+}
+
+pub async fn request_target(client: &Client<HttpConnector>) -> Result<RawSchoolDataKey, Box<dyn std::error::Error + Send + Sync>> {
+    let request = "http://comci.kr:4082/st".parse()?;
+    let mut response = client.get(request).await?;
+    
+    let mut buffer = vec![];
+    while let Some(chunk) = response.body_mut().data().await {
+        buffer.append(&mut chunk?.to_vec());
+    }
+
+    let (html, _, _) = encoding_rs::EUC_KR.decode(buffer.as_slice());
+    let re = Regex::new(r#"(?<=\$\.ajax\({ url:'\.\/)(.*)(?='\+sc,success)"#).unwrap();
+
+    let re2 = Regex::new(r#"(?<=sc_data\(')(.*)(?=',sc,1)"#).unwrap();
+
+    let re3 = Regex::new(r#"(?<=if\(th<자료\.)(.*)(?=\.length\))"#).unwrap();
+    
+    let re4 = Regex::new(r#"(?<=일일자료=자료\.)(.*)(?=\[학년\]\[반\]\[요일\]\[교시\];if\(자료\.강의실==1)"#).unwrap();
+
+    let re5 = Regex::new(r#"(?<=속성\+"'>"\+자료\.)(.*)(?=\[sb\]\+"<br>"\+성명)"#).unwrap();
+
+    let keys = RawSchoolDataKey {
+        subjects: String::from(re5.find(&html).unwrap().unwrap().as_str()),
+        teachers: String::from(re3.find(&html).unwrap().unwrap().as_str()),
+        timetable: String::from(re4.find(&html).unwrap().unwrap().as_str()),
+        encode_header: String::from(re2.find(&html).unwrap().unwrap().as_str()),
+        url_piece: String::from(re.find(&html).unwrap().unwrap().as_str())
+    };
+
+    Ok(keys)
 }
